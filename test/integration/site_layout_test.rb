744bb30450dcdbea3670c75dcb3256039ef9d9c7
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-08 19:14:24 +0200

    assert_template is deprecated, assert_select h1 should be enough

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 	test "basic layout" do
 		get root_path
-		assert_template 'members_pages/home'
 		assert_select "h1",	"welcome"
 	end
 end
