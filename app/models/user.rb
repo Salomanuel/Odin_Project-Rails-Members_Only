99f6c3b12cdb8ef56f0ec12c691ec2ef75c67606
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-10 12:54:21 +0200

    add many validation tests for username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+		EMAIL_REGEX = /\A^\w+[\w+-]+\.{0,1}[\w]+@\[*\w+[\w.-]+\w+\.\w+\]*$\z/i
+
+
+	validates_presence_of :name, :email
+	validates_length_of 	:name, 	maximum: 20
+	validates_length_of 	:email, maximum: 50
+	validates_format_of		:email, with: EMAIL_REGEX
+
 	has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,94 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@user = User.new(name:"Example User", email: "user@example.com",
+										password: "foobar", password_confirmation: "foobar")
+	end
+
+	test "it works" do
+		assert @user.valid?
+	end
+
+	# NAME 
+	test "name cannot be blank" do
+		@user.name = ""
+		assert_not @user.valid?
+	end
+
+	test "name cannot be too long" do
+		@user.name = "a" * 50
+		assert_not @user.valid?
+	end
+
+	# EMAIL
+	test "email cannot be blank" do
+		@user.email = ""
+		assert_not @user.valid?
+	end
+
+	test "email cannot be too long" do
+		@user.email = "a" * 50 + "@example.com"
+		assert_not @user.valid?
+	end
+
+	test "valid emails are valid" do
+		valid = %w(
+							email@example.com
+							firstname.lastname@example.com
+							email@subdomain.example.com
+							firstname+lastname@example.com
+							email@123.123.123.123
+							email@[123.123.123.123]
+							1234567890@example.com
+							email@example-one.com
+							_______@example.com
+							email@example.name
+							email@example.museum
+							email@example.co.jp
+							firstname-lastname@example.com)
+		valid.each 		do |e| 
+			@user.email = e
+			assert @user.valid?
+		end
+	end
+
+	test "NOT valid emails are unvalid" do
+		unvalid = %w(								plainaddress
+								#@%^%#$@#$@#.com
+								@example.com
+								email.example.com
+								email.@example.com
+								あいうえお@example.com
+								
+								email@example
+								email@-example.com
+								email@example..com
+
+								JoeSmith<email@example.com>
+
+
+
+
+)
+		unvalid.each do |e| 
+			@user.email = e
+			puts e
+			assert_not @user.valid?
+		end
+	end
+
+	# PASSWORD
+
+	test "password cannot be blank" do
+		skip
+		@user.password = ""
+		assert_not @user.valid?
+	end
+
+	test "password cannot be too short" do
+		skip
+		@user.password = "abcde"
+		assert_not @user.valid?
+	end
 end
