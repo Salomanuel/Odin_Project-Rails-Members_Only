aa2c0c36a6c9047de1efb865171ec0e9171cf72d
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-13 11:47:00 +0200

    add fixtures setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
 	has_secure_password
+
+
+	# necessary for the fixtures
+	def User.digest(string) # please refactor
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost 
+		BCrypt::Password.create(string, cost: cost)
+	end
 end
