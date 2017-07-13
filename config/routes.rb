fe479d590f7a9048732294d3cf7482c03c0bbf8d
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-13 12:01:58 +0200

    begin SessionsControllers and sessions in general

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   get '/signup', to: 'users#new'
+  get '/login' , to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   resources :users
 end
