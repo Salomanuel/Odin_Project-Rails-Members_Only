e0a1e6fba4d8eb92004f707f10aff8af3be8918f
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-15 11:22:03 +0200

    add simple tests to check the remember thing

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,6 +18,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", logout_path, count: 1
 	end
 
+	test "remember_test" do
+		log_in_as(@user, remember_me: "1")
+		assert_not_empty cookies['remember_token']
+	end
+
+	test "don't remember test" do
+		log_in_as(@user, remember_me: "1")
+		# log in again to verify that the cookie is GONE
+		log_in_as(@user, remember_me: "0")
+		assert_empty cookies['remember_token']
+	end
+
 	test "it should NOT login" do
 		post login_path, params: { session: { email: @user.email, password: "foobaz" } }
 		assert_select "form"
