471b26ea69f3977fae135f9f0e2229d55a90b4b5
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-15 10:39:11 +0200

    add test for invalid login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,4 +17,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", login_path, 	count: 0
 		assert_select "a[href=?]", logout_path, count: 1
 	end
+
+	test "it should NOT login" do
+		post login_path, params: { session: { email: @user.email, password: "foobaz" } }
+		assert_select "form"
+		assert_select "input", name: "user[email]"
+	end
 end
