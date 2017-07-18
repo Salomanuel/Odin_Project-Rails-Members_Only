21dd567a77941dd0d314c4ccc79469e12b37fdce
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-18 17:47:56 +0200

    the Posts can be created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,22 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
+
+	def create 
+		@post = Post.new(post_params)
+		@post.user_id = @current_user.id
+		if @post.save
+			redirect_to posts_path
+		else
+			render :new
+		end
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 
 end
