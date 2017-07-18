e80acb23ab964dc9a22249c3446bc97849517ed7
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-18 17:56:19 +0200

    show the posts to not logged in users, but hide the users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 include ApplicationHelper
 class PostsController < ApplicationController
-	before_action :logged_in_user
+	before_action :logged_in_user, only: [:show, :new]
 
 	def index
+		current_user
 		@posts = Post.all
 	end
 
