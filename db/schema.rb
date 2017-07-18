816d200dee8515ae954df227698544f4b4cda194
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-18 12:35:53 +0200

    generate and migrate post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170715084319) do
+ActiveRecord::Schema.define(version: 20170718103335) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",      null: false
